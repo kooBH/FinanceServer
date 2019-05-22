c62d0c0067d761a11753e76bd73cf0451f7a7d03
Bon Hyeok Koo <k01032633280@gmail.com>
2019-05-22 22:59:20 +0900

    5-22

---
+++ Analyze.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import datetime as dt 
+from statsmodels.formula.api import ols 
+import seaborn as sns;  sns.set()
+import matplotlib.pyplot as plt
+
+
+def joint(target_1,target_2,col='Close',dur=240):
+    ols_df1 = pd.read_csv('data/'+target_1 + '.csv',index_col='Date')
+    ols_df1 = ols_df1[[col]]
+    ols_df1.columns = [target_1]
+    ols_df2 = pd.read_csv('data/'+target_2 + '.csv',index_col='Date')
+    ols_df2 = ols_df2[[col]]
+    ols_df2.columns = [target_2]
+    new_df = pd.concat([ols_df1, ols_df2], axis=1, sort=False)
+    new_df = new_df.dropna()
+    new_df = new_df[-dur:]
+    ret_index = new_df.pct_change()
+    ret_index = ret_index.dropna()
+    sns.jointplot(target_1, target_2, data = ret_index, kind='reg')
--- SymbolList.py
+++ SymbolList.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import datetime as dt
-
+import fix_yahoo_finance as yf
 import os.path
+import sys
+
+sys.path.append('data/')
 
 def init_symbol_DF() :
     initial_time = dt.datetime(2010,2, 1).strftime("%Y-%m-%d")
 
     df = pd.DataFrame
 
-    csv_name = 'symbol_list.csv'
+    csv_name = 'data/symbol_list.csv'
 
     if(os.path.isfile(csv_name)):
         df = pd.read_csv(csv_name,index_col='Name')
@@ -20,7 +23,7 @@ def init_symbol_DF() :
     return df
 
 def add_symbol(df, name='Apple' ,symbol='AAPL', init=False):
-    csv_name = 'symbol_list.csv'
+    csv_name = 'data/symbol_list.csv'
     if(not name in df.index):
         initial_time = dt.datetime(2010,1, 1).strftime("%Y-%m-%d")
         df.loc[name] = [symbol,initial_time,initial_time]
@@ -31,21 +34,59 @@ def add_symbol(df, name='Apple' ,symbol='AAPL', init=False):
     
     if(init):
         symbol_list = {
-         # 미국 IT 기업
-        'Apple':'AAPL' ,
-        'Microsoft':'MSFT',
-        'IBM':'IBM',
-        'Oracle':'ORCL',
-        'Amazon':'AMZN',
-        'Tesla':'TSLA',
-        # 한국 IT 기업
-         'Samsung_Electronics':'005930.KS',
-         'SK_hynix':'000660.KS',
-        'LG_Electronics':'066570.KS',
-        'NAVER':'035420.KS'
+            # 미국 IT 기업
+            'Apple':'AAPL' ,
+            'Microsoft':'MSFT',
+            'IBM':'IBM',
+            'Oracle':'ORCL',
+            'Amazon':'AMZN',
+            'Tesla':'TSLA',
+            'Alphabet':'GOOG',
+            'Facebook':'FB',
+            'Cisco':'CSCO',
+            '타이완 반도체':'TSM',
+            'Intel':'INTC',
+            '오라클':'ORCL',
+            'SAP':'SAP',
+            # 한국 IT 기업
+            'Samsung_Electronics':'005930.KS',
+            'SK_hynix':'000660.KS',
+            'LG_Electronics':'066570.KS',
+            '삼성SDI':'006400.KS',
+            '엔씨소프트':'036570.KS',
+            '삼성SDS':'018260.KS',
+            '카카오':'035720.KS',
+            '삼성전기':'009150.KS',
+            'LG디스플레이':'034220.KS',
+            '네이버':'035420.KS'
+            # 한국 지표
+            
         }
         for k in symbol_list:
             add_symbol(df,k,symbol_list[k]) 
             
     df.to_csv(csv_name)
+    
+def download():
+    df = pd.read_csv('data/symbol_list.csv',index_col='Name')
+    list_df = [None] * len(df.index)
+    today = dt.datetime.utcnow().strftime("%Y-%m-%d")
+    for idx in range(0,len(df.index)):
+        list_df[idx] = df.index[idx]
+        print('Note::Processing  ' + list_df[idx])        
+        # 이미 최신 데이터
+        if(df.iloc[idx][2] == today):
+            print('  Pass::Alread up-to-date.')
+            continue
+        try:
+            #print(df.iloc[idx][1] + " ~ " + today)
+            temp_df = yf.download(df.iloc[idx][0], start = df.iloc[idx][1], end =  today)
+            df.iloc[idx][2] = today
+            temp_df.to_csv('data/'+list_df[idx] + '.csv')
+        except ValueError:
+            print('Value Error')
+        
+    # 심볼리스트 갱신
+    df.to_csv('data/symbol_list.csv')
+    print('Note::data/symbol_list.csv is updated.')
    
