9b8911cf09dd7053b352cebfe0cf0c6f6249ee35
kooBH <k01032633280@gmail.com>
2019-07-01 21:38:40 +0900

    comment

--- DE_PRADO/DataCollector.py
+++ DE_PRADO/DataCollector.py
@@ -3,19 +3,22 @@ import pandas as pd
 import os
 
 def fromAlphaVantage(symbol,name):
-    # My API Key 별도의 파일로 분리해야함. 
+    # 별도의 파일에서 API KEY를 가져옴. 
     f = open("ALPHA_KEY", 'r')
     key = f.readline()
     f.close()
+    # AlphaVantage에서 데이터를 받아온다.
     ts  = TimeSeries(key=key, output_format='pandas')
     tmp, meta_tmp = ts.get_intraday(symbol=symbol,interval='1min', outputsize='full')
     tmp.index = pd.to_datetime(tmp.index)
     idxs = tmp.index
+    # 일자별로 파일을 분할해서 저장한다
     tmp.index = [[str(i) for i in idxs.strftime("%Y-%m-%d")],[ str(i) for i in idxs.strftime("%H-%M-%S")]]
+    # 이름 정리
     tmp.columns = ["volume", "high","open","close","low"]
     tmp.index.names = ["date", "time"]
+    # 중복된 파일은 생성하지 않음.
     if not os.path.exists( name): os.makedirs(name)
     for byDay in tmp.index.levels[0] :
         if not os.path.exists(name+'/'+byDay + '.csv') : tmp.loc[byDay].to_csv(name+'/'+byDay + '.csv')
             
-def 
